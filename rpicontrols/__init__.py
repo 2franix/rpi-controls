16a4fd6c247e08f744ebbbf6bcfd9577f72a4604
Cyrille Defranoux <cyrille.github@defx.fr>
2022-05-13 11:00:11 +0200

    Read version from package info
    In order to have a single source of truth.

--- rpicontrols/__init__.py
+++ rpicontrols/__init__.py
@@ -16,6 +16,7 @@
 # along with rpi-controls.  If not, see <https://www.gnu.org/licenses/>.
 
 from typing import Optional
+import pkg_resources
 
 from .controller import Controller as Controller
 from .controller import Button as Button
@@ -24,7 +25,7 @@ from .gpio_driver import PullType as PullType
 
 # Define public modules and functions.
 __all__ = ["Controller", "Button", "GpioDriver", "PullType", "make_controller"]
-__version__ = "1.0.0.1"
+__version__ = pkg_resources.get_distribution("rpi-controls").version
 
 
 def make_controller(gpio_driver: Optional[GpioDriver] = None) -> Controller:
